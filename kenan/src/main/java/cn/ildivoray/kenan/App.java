0b4cc8e796df2d6cfa787e7ff2a676631285b27a
divaRay <326746848@qq.com>
2017-05-15 21:58:26 +0800

    kenan

---
+++ kenan/src/main/java/cn/ildivoray/kenan/App.java
@@ -0,0 +1,13 @@
+package cn.ildivoray.kenan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
